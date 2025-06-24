de7613d32acce5dfa359568f0955587d305b47ad
thrishasivasakthi <thrishasivasakthi@gmail.com>
2025-06-24 14:53:12 +0530

    EventDb_API

---
+++ EventDb_API/AppUI/Program.cs
@@ -0,0 +1,24 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+// Register HttpClient with matching name to your controller usage
+builder.Services.AddHttpClient("CaseStudyAPI", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7224/"); 
+});
+
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=UserInfo}/{action=LoginPage}/{id?}");
+
+app.Run();
